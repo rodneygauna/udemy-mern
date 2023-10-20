d52cf18777719f573062582b7803ff99084abf9c
Rodney Gauna <rodneygauna@gmail.com>
2023-10-20 15:19:01 -0700

    user list populating

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-  return <h2>Users works!</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Max Schwarz",
+      image:
+        "https://avatars.githubusercontent.com/u/10000101?s=460&u=1b8e5e3a3d9a3b7b4d5d4d3d2d1d0c9c8c7c6c5c4&v=4",
+      places: 3,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
