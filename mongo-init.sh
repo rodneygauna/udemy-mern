af014e8406155513c94d387e87c61402cf32026f
Rodney Gauna <rodneygauna@gmail.com>
2023-10-20 01:34:18 -0700

    mongodb is working with auth

--- mongo-init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Creating users..."
-
-mongo admin --host localhost -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD --eval "db = db.getSiblingDB('mydatabase'); db.createUser({ user: '$MONGO_USER', pwd: '$MONGO_PASSWORD', roles: [{ role: 'readWrite', db: 'mydatabase' }] });"
-
-echo "Users created."
