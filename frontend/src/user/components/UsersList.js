85ad5c32247889a531a7a061d750343e00002caf
Rodney Gauna <rodneygauna@gmail.com>
2023-10-23 07:50:25 -0700

    apply css styling

--- frontend/src/user/components/UsersList.js
+++ frontend/src/user/components/UsersList.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 import "./UsersList.css";
+import Card from "../../shared/components/UIElements/Card";
 import UserItem from "./UserItem";
 
 const UsersList = (props) => {
   if (props.items.length === 0) {
     return (
       <div className="center">
-        <h2>No users found.</h2>
+        <Card>
+          <h2>No users found.</h2>
+        </Card>
       </div>
     );
   }
