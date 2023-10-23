3157e876c0e4eaa9afeb1f83dcae42f48c9aa2d4
Rodney Gauna <rodneygauna@gmail.com>
2023-10-23 10:14:30 -0700

    added Google Maps SDK placeholder (sec 57)

--- frontend/src/places/components/PlaceItem.js
+++ frontend/src/places/components/PlaceItem.js
@@ -4,6 +4,7 @@ import "./PlaceItem.css";
 import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/FormElements/Button";
 import Modal from "../../shared/components/UIElements/Modal";
+import Map from "../../shared/components/UIElements/Map";
 
 const PlaceItem = (props) => {
   const [showMap, setShowMap] = useState(false);
@@ -22,6 +23,7 @@ const PlaceItem = (props) => {
         footer={<Button onClick={closeMapHandler}>CLOSE</Button>}
       >
         <div className="map-container">
+          {/*Google Maps SDK: <Map center={props.coordinates} zoom={16} /> */}
           <h2>THE MAP</h2>
         </div>
       </Modal>
