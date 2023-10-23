7cc3477da9f4139c16349679dbd86b4ed317ce2b
Rodney Gauna <rodneygauna@gmail.com>
2023-10-23 08:03:52 -0700

    nav and header working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import NoMatch from "./nomatch/pages/NoMatch";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Users />} />
-        <Route path="/places/new" element={<NewPlace />} />
-        <Route path="*" element={<NoMatch />} />
-      </Routes>
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/places/new" element={<NewPlace />} />
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+      </main>
     </BrowserRouter>
   );
 };
